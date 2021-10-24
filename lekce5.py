4399c984c2429982b7f5836c5aac47e0696a5b54
Pavel Prchal <prchalp@gmail.com>
2021-10-24 10:39:55 +0200

    oprava

--- lekce5.py
+++ lekce5.py
@@ -19,7 +19,7 @@ import matplotlib.pyplot as plt
 osaX = []
 osaY = []
 
-kroku = 20
+kroku = 40
 kousekX =  (2 * math.pi) / kroku        # počítáme 360 stupňů...
 hlaska = "kousekX: " + str(kousekX)     # sečtení jablko a jablko -- str() převede číslo na řetězec
 print(hlaska)
@@ -28,30 +28,21 @@ for krok in range(kroku):
     osaX.append(x)
     y = math.sin(x)
     osaY.append(y)
+    druhaHlaska = "krok[" + str(krok) + "] X: " + str(x) + " Y: " + str(y)
 
     x = x + kousekX
-    druhaHlaska = "krok[" + str(krok) + "] X: " + str(x) + "Y: " + str(y)
     print(druhaHlaska)
 
 
-#        
-plt.plot(osaX, osaY, 'ro')
+# kreslíme graf       
+plt.plot(osaX, osaY)
+
+# zkus 
+# plt.plot(osaX, osaY, 'ro')
+
+
 # plt.axis([0, 6, 0, 20])
 plt.show()
-
-# Data for plotting
-# t = np.arange(0.0, 2.0, 0.01)
-# s = 1 + np.sin(2 * np.pi * t)
-
-# fig, ax = plt.subplots()
-# ax.plot(t, s)
-
-# ax.set(xlabel='time (s)', ylabel='voltage (mV)',
-#        title='About as simple as it gets, folks')
-# ax.grid()
-
-# fig.savefig("test.png")
-# plt.show()
 
 
 
